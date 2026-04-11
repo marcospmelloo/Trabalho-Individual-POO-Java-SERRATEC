4a092eb59b045383fd0c1d6cb791ca06b916542a
marcospmelloo <marcospoliveiramello@gmail.com>
2026-04-10 21:41:42 -0300

    exercicio 2 - conta bancaria

---
+++ exercicio2-conta-bancaria/src/ContaBancaria.java
@@ -0,0 +1,56 @@
+public class ContaBancaria {
+    private int numero;
+    private String titular;
+    private double saldo;
+    private int contSaque = 0;
+
+    public ContaBancaria(int numero, String titular, double saldo) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo + 50.0;
+    }
+
+    public void depositar(double deposito){
+        saldo += deposito;
+    }
+
+    public boolean sacar(double saque){
+        if(contSaque >= 3) {
+            System.out.println("Operação inválida! Limite de saques diários atingido!");
+            return false;
+        }
+        if(saque > 1000.0){
+            System.out.println("Você não pode sacar mais que R$1000.00");
+            return false;
+        }
+        if(saque > saldo){
+            System.out.println("Saldo insuficiente!");
+            return false;
+        }
+
+        saldo -= saque;
+        contSaque++;
+        System.out.println("Saque realizado com sucesso!");
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Número da conta: " + numero +
+                ", Titular: " + titular +
+                ", Saldo : R$" + saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    }
---
+++ exercicio2-conta-bancaria/src/Main.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double saldoTotal = 0;
+        int opcao = 0, contSaque = 0;
+
+        System.out.println("Seja Bem-Vindo ao Banco Itaú!");
+        System.out.println(" ");
+        System.out.print("Digite o número da sua conta: ");
+        int numero = sc.nextInt();
+        System.out.print("Digite o seu nome: ");
+        sc.nextLine();
+        String titular = sc.nextLine();
+        System.out.println("Parabéns! Você acabou de ganhar um bônus de R$50,00 " +
+                "por ter aberto uma nova conta!");
+        System.out.println(" ");
+        ContaBancaria conta = new ContaBancaria(numero, titular, saldoTotal);
+
+        System.out.println("Selecione uma das opções abaixo:");
+
+        System.out.println(" ");
+
+        do{
+            System.out.println("1 - Ver Saldo");
+            System.out.println("2 - Depositar");
+            System.out.println("3 - Sacar");
+            System.out.println("0 - Sair");
+            opcao = sc.nextInt();
+
+            switch(opcao){
+                case 1:
+                    System.out.println("Seu saldo atual é de: " + conta.getSaldo());
+                    System.out.println(" ");
+                    break;
+                case 2:
+                    System.out.println("DEPÓSITO");
+                    System.out.println("Quanto você deseja depositar?");
+                    double deposito = sc.nextDouble();
+                    conta.depositar(deposito);
+                    System.out.println("Depósito realizado com sucesso!");
+                    System.out.println(" ");
+                    break;
+                case 3:
+                    System.out.println("SAQUE");
+                    System.out.println("Quanto você deseja sacar?");
+                    double saque = sc.nextDouble();
+                    conta.sacar(saque);
+                    System.out.println(" ");
+                    break;
+                case 0:
+                    System.out.println("Encerrando o atendimento...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }while(opcao != 0);
+
+        System.out.println(conta.toString());
+
+        sc.close();
+    }
+}
