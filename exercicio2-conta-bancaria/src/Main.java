210a87c7122036c306db693e4aa5e67b40e3801d
marcospmelloo <marcospoliveiramello@gmail.com>
2026-04-17 00:26:32 -0300

    Correção de sintaxe

--- exercicio2-conta-bancaria/src/Main.java
+++ exercicio2-conta-bancaria/src/Main.java
@@ -8,7 +8,8 @@ public class Main {
         int opcao = 0, contSaque = 0;
 
         System.out.println("Seja Bem-Vindo ao Banco Itaú!");
-        System.out.println(" ");
+        System.out.println();
+
         System.out.print("Digite o número da sua conta: ");
         int numero = sc.nextInt();
         System.out.print("Digite o seu nome: ");
@@ -16,14 +17,11 @@ public class Main {
         String titular = sc.nextLine();
         System.out.println("Parabéns! Você acabou de ganhar um bônus de R$50,00 " +
                 "por ter aberto uma nova conta!");
-        System.out.println(" ");
+        System.out.println();
         ContaBancaria conta = new ContaBancaria(numero, titular, saldoTotal);
 
-        System.out.println("Selecione uma das opções abaixo:");
-
-        System.out.println(" ");
-
         do{
+            System.out.println("Selecione uma das opções abaixo:");
             System.out.println("1 - Ver Saldo");
             System.out.println("2 - Depositar");
             System.out.println("3 - Sacar");
@@ -33,7 +31,7 @@ public class Main {
             switch(opcao){
                 case 1:
                     System.out.println("Seu saldo atual é de: " + conta.getSaldo());
-                    System.out.println(" ");
+                    System.out.println();
                     break;
                 case 2:
                     System.out.println("DEPÓSITO");
@@ -41,14 +39,14 @@ public class Main {
                     double deposito = sc.nextDouble();
                     conta.depositar(deposito);
                     System.out.println("Depósito realizado com sucesso!");
-                    System.out.println(" ");
+                    System.out.println();
                     break;
                 case 3:
                     System.out.println("SAQUE");
                     System.out.println("Quanto você deseja sacar?");
                     double saque = sc.nextDouble();
                     conta.sacar(saque);
-                    System.out.println(" ");
+                    System.out.println();
                     break;
                 case 0:
                     System.out.println("Encerrando o atendimento...");
@@ -58,6 +56,7 @@ public class Main {
             }
         }while(opcao != 0);
 
+        System.out.println();
         System.out.println(conta.toString());
 
         sc.close();
