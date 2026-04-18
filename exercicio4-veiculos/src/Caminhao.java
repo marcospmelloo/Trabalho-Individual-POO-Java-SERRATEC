9932bdeb6cd9343daa0485336ff8597b2d3c9da5
marcospmelloo <marcospoliveiramello@gmail.com>
2026-04-17 22:18:39 -0300

    Adicionando regras de negócio

--- exercicio4-veiculos/src/Caminhao.java
+++ exercicio4-veiculos/src/Caminhao.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.util.Date;
+
 public final class Caminhao extends Veiculo{
     private final double capacidadeCargaToneladas;
 
@@ -7,5 +10,23 @@ public final class Caminhao extends Veiculo{
         this.capacidadeCargaToneladas = capacidadeCargaToneladas;
     }
 
+    @Override
+    public double alugarVeiculo(double pesoCarga, int dias) {
+        double total = getValorLocacaoDiaria() * dias;
+        if (pesoCarga > capacidadeCargaToneladas) {
+            total += total * 0.10;
+        }
+        return total;
+    }
+
+    @Override
+    public double calcularIPVA() {
+        if(getAnoFabricacao() < (LocalDate.now().getYear() - 20)){
+            return 0.0;
+        }
+        else {
+            return getPrecoFipe() * 0.015;
+        }
+    }
 
 }
--- exercicio4-veiculos/src/CarroPasseio.java
+++ exercicio4-veiculos/src/CarroPasseio.java
@@ -1,5 +1,23 @@
+import java.time.LocalDate;
+
 public final class CarroPasseio extends Veiculo{
-    public CarroPasseio(String placa, String marca, double valorLocacaoDiaria, int anoFabricacao, double precoFipe) {
+
+    public CarroPasseio(String placa, String marca, double valorLocacaoDiaria,
+                        int anoFabricacao, double precoFipe) {
         super(placa, marca, valorLocacaoDiaria, anoFabricacao, precoFipe);
+    }
+
+    @Override
+    public double alugarVeiculo(double pesoCarga, int dias) {
+        return getValorLocacaoDiaria() * dias;
+    }
+
+    @Override
+    public double calcularIPVA() {
+        if (getAnoFabricacao() < (LocalDate.now().getYear() - 20)) {
+            return 0.0;
+        } else {
+            return getPrecoFipe() * 0.04;
+        }
     }
 }
